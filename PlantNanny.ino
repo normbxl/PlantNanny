d879f90c577ac4e21cfb5c3e8d19d0a9077b987e
Norman Liebich <norm@studioadhoc.de>
2015-07-13 21:35:21 +0200

    First rough working one

--- PlantNanny.ino
+++ PlantNanny.ino
@@ -12,10 +12,11 @@
 #define MOISTURE_PIN_2 A2
 #define PUMP_PIN A3
 
-#define MOISTURE_PUMP_THRESHOLD 300
 
 #define WIFI_SSID "lovely-N"
 #define WIFI_PWD "d0tlab1$$up1"
+
+unsigned int MOISTURE_PUMP_THRESHOLD = 300;
 
 OLED oled;
 
@@ -52,6 +53,7 @@ unsigned long nextPumpTS = oneDay; // 24 Stunden
 long dayAberration = 0L;
 
 byte tickCounter = 0;
+volatile boolean tick = false;
 
 // Hardware-Serial > SoftwareSerial pass-through
 //void serialEvent() {
@@ -174,10 +176,11 @@ ISR(TIMER1_COMPA_vect) {
 		Tavg = (Tnow * Talpha) + Tavg*(1.0 - Talpha);
 	}
 
-	moisture[0] = (int)((float)analogRead(MOISTURE_PIN_1) * moistureAlpha + (float)moisture[0]*(1.0 - moistureAlpha));
+	moisture[0] = (int)((float)analogRead(MOISTURE_PIN_1) * moistureAlpha + (float)moisture[0] * (1.0 - moistureAlpha));
 	moisture[1] = (int)((float)analogRead(MOISTURE_PIN_2) * moistureAlpha + (float)moisture[1] * (1.0 - moistureAlpha));
 	now++;
 	digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+	tick = true;
 }
 
 float readTemperature()
@@ -193,124 +196,159 @@ float readTemperature()
 unsigned long sendTS = 0L;
 bool waitingForResponse = false;
 
-int freeRam() {
-	extern int __heap_start, *__brkval;
-	int v;
-	return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
-}
-
-void loop()
-{
-	if (sendTS == 0L || millis() - sendTS > 60000) {
-		oled.clear();
-		oled.setCursor(0, 0);
-		oled.print("TX ");
-		int err = http.get(serverIP, "192.168.1.43", "/time.php");
-		if (err > 0) {
-			oled.print("c:");
-			oled.print(err);
-			oled.print("    ");
-		}
-		else {
-			oled.print("#");
-			oled.print(err);
-		}
-		sendTS = millis();
-		waitingForResponse = true;
+void sendSensorData() {
+	String url = F("/d-parc/fb_flora_feed/update.php?cmd=save&s[0][id]=1&s[0][t]=0&s[1][id]=2&s[1][t]=0&s[2][id]=3&s[2][t]=1&s[0][v]=");
+	url.concat(moisture[0]);
+	url.concat("&s[1][v]=");
+	url.concat(moisture[1]);
+	url.concat("&s[2][v]=");
+	url.concat(Tavg);
+	oled.setCursor(0, 0);
+	oled.print("TX ");
+	int err = http.get(serverIP, "192.168.1.43", url.c_str());
+	if (err > 0) {
+		oled.print("c:");
+		oled.print(err);
+		oled.print(" ");
 	}
-
-	if (waitingForResponse && http.available()) {
-		oled.setCursor(0, 0);
-		oled.print("RX ");
-		int err = http.responseStatusCode();
+	else {
 		oled.print("#");
 		oled.print(err);
-		if (err == 200) {
-		
-			TRACE("> Skipping Headers: ");
-			
-			int res = http.skipResponseHeaders();
-			TRACE(res);
-			int length = http.contentLength();
-			TRACE("> bytes received: ");
-			TRACE(length);
-			uint8_t* buffer = (uint8_t*)malloc(length);
+	}
+	sendTS = now;
+	waitingForResponse = true;
+}
+
+void receiveHttp() {
+	waitingForResponse = false;
+
+	oled.setCursor(0, 0);
+	oled.print("RX ");
+	int err = http.responseStatusCode();
+	oled.print("#");
+	oled.print(err);
+	if (err == 200) {
+		int res = http.skipResponseHeaders();
+		int length = http.contentLength();
+		uint8_t* buffer = (uint8_t*)malloc(length+1);
+		if (buffer != NULL) {
 			http.readBytes(buffer, length);
+
 			// Terminate String
 			buffer[length] = '\0';
 
 			oled.clear();
 			oled.setCursor(0, 0);
 			oled.print(">");
-			oled.print((char*)buffer);
-			TRACE("HTTP-Content: ");
+			// oled.print((char*)buffer);
+
+			TRACE(F("HTTP-Content: "));
 			TRACE((char*)buffer);
+
+			parseReceivedData((char*)buffer);
+
+			free(buffer);
+
 		}
 		else {
-			oled.print("r:");
-			oled.print(err);
+			oled.print("MALLOC ");
 		}
-		http.stop();
-		waitingForResponse = false;
+	}
+	else {
+		oled.print("r:");
+		oled.print(err);
+	}
+	http.stop();
+	
+}
+
+void parseReceivedData(const char* data) {
+	String str(data);
+	int iEqual = -1;
+	do {
+		int newEqual = str.indexOf('=', iEqual+1);
+		if (newEqual > -1) {
+			String varname = str.substring(iEqual + 1, newEqual);
+			int valueIndex = str.indexOf('&', newEqual);
+			String value = str.substring(newEqual + 1, valueIndex == -1 ? str.length() : valueIndex);
+			newEqual += value.length() + 1;
+			TRACE(varname + " = " + value);
+			if (varname == F("result")) {
+				oled.print(value.c_str());
+			}
+			else if (varname == F("pump_threshold")) {
+				int newThreshold = value.toInt();
+				if (newThreshold > 0) {
+					MOISTURE_PUMP_THRESHOLD = (uint16_t)newThreshold;
+				}
+			}
+			else if (varname == F("cmd")) {
+				if (value == F("pump_now")) {
+					nextPumpTS = now + dayAberration;
+				}
+			}
+		}
+		iEqual = newEqual;
+	} 
+	while (iEqual != -1);
+}
+
+void loop()
+{
+	if (sendTS == 0L || now - sendTS > 300) {
+		sendSensorData();
+	}
+
+	if (waitingForResponse && http.available()) {
+		receiveHttp();
+	}
+
+	if (tick) {
+		tick = false;
+
+		oled.setCursor(0, 1);
+		oled.print(Tavg, 0);
+		oled.print(' ');
+		switch (state) {
+		case IDLE:
+			if (now > nextPumpTS - dayAberration) {
+				state = PUMP;
+				oled.setCursor(0, 1);
+				if (moisture[0] < MOISTURE_PUMP_THRESHOLD && moisture[1] < MOISTURE_PUMP_THRESHOLD) {
+					digitalWrite(PUMP_PIN, HIGH);
+					digitalWrite(13, HIGH);
+					oled.print("pumping     ");
+				}
+				else {
+					oled.print("pump suspend");
+				}
+				lastPumpTS = now;
+			}
+			else {
+				float abFac = (exp((17.62 * Tavg) / (273.12 + Tavg))) / 20.0; // Null-wert bei avg 15 °C
+				dayAberration = (long)(abFac*(float)oneDay);
+
+				long Tdiff = (long)(nextPumpTS - dayAberration) - now;
+				// oled.setCursor(0, 1);
+				oled.print(Tdiff / 3600L);
+				oled.print(":");
+				oled.print((Tdiff % 3600L) / 60L);
+				oled.print(":");
+				oled.print((Tdiff % 3600L) % 60L);
+			}
+			break;
+		case PUMP:
+			if (now > lastPumpTS + 65L) {
+				state = IDLE;
+				digitalWrite(PUMP_PIN, LOW);
+				digitalWrite(13, LOW);
+				oled.clear();
+				nextPumpTS += oneDay;
+			}
+			break;
+		}
 	}
 	
-	return;
-
-	//char strOut[10];
-	//oled.setCursor(0, 0);
-	//if (now % 3 == 0) {
-	//	oled.print("T now: ");
-	//	oled.print(Tnow, 1);
-	//}
-	//else if (now % 3 == 1) {
-	//	oled.print("T avg: ");
-	//	oled.print(Tavg);
-	//}
-	//else {
-	//	oled.print("Mst: ");
-	//	oled.print(moisture[0] - MOISTURE_PUMP_THRESHOLD);
-	//}
-	//oled.print("   ");
-	//switch (state) {
-	//case IDLE:
-	//	if (now > nextPumpTS - dayAberration) {
-	//		state = PUMP;
-	//		oled.setCursor(0, 1);
-	//		if (moisture[0] < MOISTURE_PUMP_THRESHOLD && moisture[1] < MOISTURE_PUMP_THRESHOLD) {
-	//			digitalWrite(PUMP_PIN, HIGH);
-	//			digitalWrite(13, HIGH);
-	//			oled.print("pumping");
-	//		}
-	//		else {
-	//			oled.print("pump suspend");
-	//		}
-	//		lastPumpTS = now;
-	//	}
-	//	else {
-	//		float abFac = (exp((17.62 * Tavg) / (273.12 + Tavg))) / 20.0; // Null-wert bei avg 15�C
-	//		dayAberration = (long)(abFac*(float)oneDay);
-
-	//		long Tdiff = (long)(nextPumpTS - dayAberration) - now;
-	//		oled.setCursor(0, 1);
-	//		oled.print(Tdiff/3600L);
-	//		oled.print(":");
-	//		oled.print((Tdiff % 3600L) / 60L);
-	//		oled.print(":");
-	//		oled.print((Tdiff % 3600L) % 60L);
-	//	}
-	//	break;
-	//case PUMP:
-	//	if (now > lastPumpTS + 65L) {
-	//		state = IDLE;
-	//		digitalWrite(PUMP_PIN, LOW);
-	//		digitalWrite(13, LOW);
-	//		oled.clear();
-	//		nextPumpTS += oneDay;
-	//	}
-	//	break;
-	//}
-
-	//delay(1000);
 }
 char* wifiStatusString(ESP8266CommandStatus status) {
 	switch (status) {
